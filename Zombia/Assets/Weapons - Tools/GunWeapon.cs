8bec8591b23444251de8c9e195e3f34e9e4a447b
Nam Sơn <loginson140521@gmail.com>
2025-06-07 11:03:15 +0700

    test animation riging

--- Zombia/Assets/Weapons - Tools/GunWeapon.cs
+++ Zombia/Assets/Weapons - Tools/GunWeapon.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:ba0e5e4192433b4a4de43840289875f08cb9e715172200ff7cbe18caa8d9a557
-size 1331
+oid sha256:3f9e0d61ad371c5e91c18ce8461c52f7b69ea119127503177626ad77259b521d
+size 1335
